155234d61219f11d279d506e3070fa305f68cbe4
Chris <chris@adadevelopersacademy.org>
2017-04-05 21:04:18 -0700

    Added logging.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
 
   def index
     tasks = Task.all
-    logger.log "Tasks: #{tasks.as_json}"
+    logger.log "Tasks: #{tasks}"
     render :json => tasks, status: :ok
   end
 
