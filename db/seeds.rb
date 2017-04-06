9884e1657dd253596889f13f264ef5a579e67aff
Chris <chris@adadevelopersacademy.org>
2017-04-05 20:13:40 -0700

    Added seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tasks = [{title: "Learn Backbone!", description: "Backbone is a cool JavaScript library.", completed: false}, {title: "Study Ruby on Rails", description: "Rails is a great way to learn to develop web APIs.", completed: true}, {title: "Buy more Dr. Pepper", description: "Nectar of the Gods", completed: false}]
+
+
+tasks.each do |task|
+  Task.create(task)
+end
